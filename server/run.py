70b044f2c9f43ae79414c19225bf8fa228d03d1e
YaozhengWang <wyaozheng@gmail.com>
2022-10-31 14:30:39 +0800

    Add Makefile for server

--- server/run.py
+++ server/run.py
@@ -4,5 +4,8 @@ from config import *
 args = parse_args()
 app = create_app(parse_config(args.config))
 
+def main():
+    app.run(host='0.0.0.0', debug=False)
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+    main()
